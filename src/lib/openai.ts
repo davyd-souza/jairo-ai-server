963be1103f37f62172368cc17941c2544a13f6ec
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-09-17 16:57:29 -0300

    feat: create create transcription route

---
+++ src/lib/openai.ts
@@ -0,0 +1,6 @@
+// DEPENDENCY
+import { OpenAI } from 'openai'
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+})
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 // DEPENDENCY
+import 'dotenv/config'
 import { fastify } from 'fastify'
 
 // ROUTE
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcrition'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app
   .listen({ port: 3333 })
