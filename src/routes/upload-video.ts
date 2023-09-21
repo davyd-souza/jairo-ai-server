137a5e10e8045a87526149853866c50bfd3d6b61
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-09-21 00:34:26 -0300

    feat: upload file to firebase

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -1,21 +1,30 @@
 // DEPENDENCY
 import path from 'node:path'
-import fs from 'node:fs'
 import { randomUUID } from 'node:crypto'
 import { pipeline } from 'node:stream'
 import { promisify } from 'node:util'
 import { fastifyMultipart } from '@fastify/multipart'
+import admin from 'firebase-admin'
+import type { FastifyInstance } from 'fastify'
 
 // TYPE
-import type { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 
 const pump = promisify(pipeline)
 
+admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    projectId: process.env.FIREBASE_APP_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  storageBucket: 'gs://jairo-ai.appspot.com/',
+})
+
 export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, {
     limits: {
-      fieldSize: 1_048_576 * 25, // 25mb
+      fieldSize: 1_048_576 * 10, // 10mb
     },
   })
 
@@ -36,18 +45,28 @@ export async function uploadVideoRoute(app: FastifyInstance) {
 
     const fileBasename = path.basename(data.filename, extension)
     const fileUploadName = `${fileBasename}-${randomUUID()}-${extension}`
-    const uploadDestination = path.resolve(
-      __dirname,
-      '../../tmp',
-      fileUploadName,
-    )
 
-    await pump(data.file, fs.createWriteStream(uploadDestination))
+    const bucket = admin.storage().bucket()
+    const file = bucket.file(fileUploadName)
+
+    const writeStream = file.createWriteStream({
+      metadata: {
+        contentType: data.mimetype,
+      },
+      resumable: false,
+    })
+
+    await pump(data.file, writeStream)
+
+    const fileUrl = await file.getSignedUrl({
+      action: 'read',
+      expires: Date.now() + 300 * 24 * 60 * 60 * 1000, // 300 days
+    })
 
     const video = await prisma.video.create({
       data: {
         name: data.filename,
-        path: uploadDestination,
+        path: fileUrl[0],
       },
     })
 
