a526b906f05f331eddac4ce3707908c35bfdf64d
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-09-26 13:17:47 -0300

    chore: allow any origin cors

--- src/server.ts
+++ src/server.ts
@@ -16,19 +16,10 @@ const app = fastify()
 app.register(multer.contentParser)
 
 app.register(fastifyCors, {
-  origin: ['https://jairo-ai.vercel.app'],
+  origin: '*',
+  // origin: 'https://jairo-ai.vercel.app',
   credentials: true,
-  allowedHeaders: [
-    'X-CSRF-Token',
-    'X-Requested-With',
-    'Accept',
-    'Accept-Version',
-    'Content-Length',
-    'Content-MD5',
-    'Content-Type',
-    'Date',
-    'X-Api-Version',
-  ],
+  allowedHeaders: '*',
   methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
 })
 
